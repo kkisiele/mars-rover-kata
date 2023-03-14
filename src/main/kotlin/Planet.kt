380bb51afcf300a2b9a5bab41dbed2763275759c
Karol Kisielewski <kkisiele@gmail.com>
2023-03-14 20:54:07 +0100

    Add extension method to Coordinates to wrap around edges

--- src/main/kotlin/Planet.kt
+++ src/main/kotlin/Planet.kt
@@ -2,14 +2,17 @@ class Planet(private val obstacles: Set<Coordinates>) {
     private val height = 10
     private val width = 10
 
-    fun nextLocation(location: Coordinates, direction: Direction) = when (direction) {
-        Direction.NORTH -> Coordinates(location.x, (location.y + 1).wrapAround(height))
-        Direction.EAST -> Coordinates((location.x + 1).wrapAround(width), location.y)
-        Direction.SOUTH -> Coordinates(location.x, (location.y - 1).wrapAround(height))
-        Direction.WEST -> Coordinates((location.x - 1).wrapAround(width), location.y)
+    fun nextLocation(location: Coordinates, direction: Direction): Coordinates {
+        val nextLocation = when (direction) {
+            Direction.NORTH -> Coordinates(location.x, location.y + 1)
+            Direction.EAST -> Coordinates(location.x + 1, location.y)
+            Direction.SOUTH -> Coordinates(location.x, location.y - 1)
+            Direction.WEST -> Coordinates(location.x - 1, location.y)
+        }
+        return nextLocation.wrapAroundEdges()
     }
 
-    private fun Int.wrapAround(total: Int): Int = (this + total) % total
-
     fun hasObstacle(location: Coordinates) = location in obstacles
+
+    private fun Coordinates.wrapAroundEdges() = Coordinates((this.x + width) % width, (this.y + height) % height)
 }
