9b20ae23f5c08654bfadc36d329038967be94def
Karol Kisielewski <kkisiele@gmail.com>
2023-03-14 13:19:08 +0100

    Refactor location wrapping around grid

--- src/main/kotlin/Planet.kt
+++ src/main/kotlin/Planet.kt
@@ -3,11 +3,13 @@ class Planet(private val obstacles: Set<Coordinates>) {
     private val width = 10
 
     fun nextLocation(location: Coordinates, direction: Direction) = when (direction) {
-        Direction.NORTH -> Coordinates(location.x, (location.y + 1) % height)
-        Direction.EAST -> Coordinates((location.x + 1) % width, location.y)
-        Direction.SOUTH -> Coordinates(location.x, if (location.y > 0) location.y - 1 else height - 1)
-        Direction.WEST -> Coordinates(if (location.x > 0) location.x - 1 else width - 1, location.y)
+        Direction.NORTH -> Coordinates(location.x, (location.y + 1).wrapAround(height))
+        Direction.EAST -> Coordinates((location.x + 1).wrapAround(width), location.y)
+        Direction.SOUTH -> Coordinates(location.x, (location.y - 1).wrapAround(height))
+        Direction.WEST -> Coordinates((location.x - 1).wrapAround(width), location.y)
     }
+
+    private fun Int.wrapAround(total: Int): Int = (this + total) % total
 
     fun hasObstacle(location: Coordinates) = location in obstacles
 }
