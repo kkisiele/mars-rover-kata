967423f3cbf02a7c17f232f315069d84146476b6
Karol Kisielewski <kkisiele@gmail.com>
2023-03-20 19:45:18 +0100

    Add factory method

--- src/main/kotlin/Planet.kt
+++ src/main/kotlin/Planet.kt
@@ -2,17 +2,14 @@ class Planet(private val obstacles: Set<Coordinates>) {
     private val height = 10
     private val width = 10
 
-    fun nextLocation(location: Coordinates, direction: Direction): Coordinates {
-        val nextLocation = when (direction) {
-            Direction.NORTH -> Coordinates(location.x, location.y + 1)
-            Direction.EAST -> Coordinates(location.x + 1, location.y)
-            Direction.SOUTH -> Coordinates(location.x, location.y - 1)
-            Direction.WEST -> Coordinates(location.x - 1, location.y)
-        }
-        return nextLocation.wrapAroundEdges()
+    fun nextLocation(location: Coordinates, direction: Direction): Coordinates = when (direction) {
+        Direction.NORTH -> wrapAroundEdges(location.x, location.y + 1)
+        Direction.EAST -> wrapAroundEdges(location.x + 1, location.y)
+        Direction.SOUTH -> wrapAroundEdges(location.x, location.y - 1)
+        Direction.WEST -> wrapAroundEdges(location.x - 1, location.y)
     }
 
-    fun hasObstacle(location: Coordinates) = location in obstacles
+    private fun wrapAroundEdges(x: Int, y: Int) = Coordinates((x + width) % width, (y + height) % height)
 
-    private fun Coordinates.wrapAroundEdges() = Coordinates((this.x + width) % width, (this.y + height) % height)
+    fun hasObstacle(location: Coordinates) = location in obstacles
 }
