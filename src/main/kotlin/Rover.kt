6887d7f4ac9e4a53e061f3e5f94d1a588e61cc8f
Karol Kisielewski <kkisiele@gmail.com>
2023-03-20 19:49:29 +0100

    Remove unnecessary this keyword

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -1,13 +1,13 @@
 data class Rover(val location: Coordinates, val direction: Direction, val obstacleEncountered: Boolean = false) {
-    fun rotateRight() = this.copy(direction =  direction.onRight)
+    fun rotateRight() = copy(direction =  direction.onRight)
 
-    fun rotateLeft() = this.copy(direction = direction.onLeft)
+    fun rotateLeft() = copy(direction = direction.onLeft)
 
     fun move(planet: Planet): Rover {
-        val nextLocation = planet.nextLocation(this.location, this.direction)
+        val nextLocation = planet.nextLocation(location, direction)
         if (planet.hasObstacle(nextLocation)) {
-            return this.copy(obstacleEncountered = true)
+            return copy(obstacleEncountered = true)
         }
-        return this.copy(location = nextLocation)
+        return copy(location = nextLocation)
     }
 }
